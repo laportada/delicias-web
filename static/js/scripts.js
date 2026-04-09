41b924cc68491600f574092739ab871b86375d80
Anibal Sierra <anibalsilei@gmail.com>
2026-04-09 02:14:47 -0400

    Add prototype HTML/CSS: styles, improved home, menu, quienes_somos templates

---
+++ static/js/scripts.js
@@ -0,0 +1,8 @@
+// static/js/scripts.js
+// JavaScript básico para interactividad futura
+console.log('Delicias Web loaded');
+
+// Ejemplo: Alert en botón (puedes agregar botones)
+function showAlert(message) {
+    alert(message);
+}
