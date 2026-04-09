fb302504cb83bbcd6daf1532e669ff84ae9e8e65
Anibal Sierra <anibalsilei@gmail.com>
2026-04-09 02:21:03 -0400

    Remove old files

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-# app/__init__.py
-from flask import Flask
-from config import Config
-
-app = Flask(__name__)
-app.config.from_object(Config)
-
-# Importar rutas después de crear la app para evitar importaciones circulares
-from app import routes
--- config.py
+++
@@ -1,7 +0,0 @@
-# config.py
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'tu_clave_secreta_aqui'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://usuario:password@localhost/delicias_web'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
